900786d927417c83b22d3941491a90bea85957dd
BobLanners <bob_lanners@yahoo.com>
2025-04-16 11:54:46 +0200

    Update plugin package: helloworld

--- sdkjs-plugins/content/helloworld/scripts/helloworld.js
+++ sdkjs-plugins/content/helloworld/scripts/helloworld.js
@@ -7,3 +7,10 @@ window.Asc.plugin.init = function () {
 window.Asc.plugin.button = function () {
     console.log("🔘 Plugin button clicked");
 };
+
+window.Asc.plugin.button = function () {
+    console.log("Run button clicked!");
+    // Example action for spreadsheet
+    var sheet = Api.GetActiveSheet();
+    sheet.GetRange("A1").SetValue("Button was clicked!");
+  };
