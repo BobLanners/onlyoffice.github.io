0ca7a50eb845b2bf4c68b7839ece6e74715fa351
BobLanners <bob_lanners@yahoo.com>
2025-05-15 15:40:39 +0200

    Update plugin package: helloworld

--- sdkjs-plugins/content/helloworld/scripts/helloworld.js
+++ sdkjs-plugins/content/helloworld/scripts/helloworld.js
@@ -1,17 +1,5 @@
-window.Asc.plugin.init = function () {
-  console.log("🧩 Plugin initialized");
-
-  document.getElementById("pasteButton").addEventListener("click", function () {
-    const text = document.getElementById("userInput").value;
-
-    Asc.scope.inputText = text;
-
-    window.Asc.plugin.callCommand(function () {
-      const sheet = Api.GetActiveSheet();
-      const value = Asc.scope.inputText || "";
-      sheet.GetRange("A1").SetValue(value);
-    }, true);
-  });
+window.Asc.plugin.button = function () {
+  Api.GetActiveSheet().GetRange("A1").SetValue("Hardcoded test");
 };
 
 window.Asc.plugin.button = function () {
