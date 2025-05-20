4e26790d8cdb5cecdc082b020093f83bef90aa15
BobLanners <bob_lanners@yahoo.com>
2025-05-20 15:24:17 +0200

    Update plugin package: helloworld

--- sdkjs-plugins/content/helloworld/scripts/paste.js
+++ sdkjs-plugins/content/helloworld/scripts/paste.js
@@ -1,8 +1,6 @@
 window.Asc.plugin.init = function () {
-  document.getElementById("pasteButton").addEventListener("click", function () {
-    window.Asc.plugin.callCommand(function () {
-      const sheet = Api.GetActiveSheet();
-      sheet.GetRange("A1").SetValue("Hello World");
-    }, true);
-  });
+  window.Asc.plugin.callCommand(function () {
+    const sheet = Api.GetActiveSheet();
+    sheet.GetRange("A1").SetValue("Test from init");
+  }, true);
 };
